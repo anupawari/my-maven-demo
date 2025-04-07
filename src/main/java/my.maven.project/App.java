140a86600613a29603500dba137a54bfdaacc537
anupawari <awarianup2@gmail.com>
2025-04-07 09:56:44 +0530

    Updated

--- src/main/java/my.maven.project/App.java
+++ src/main/java/my.maven.project/App.java
@@ -1,4 +1,4 @@
-package my-maven-project;
+package my.maven.project;
 
 /**
  * Hello world!
--- src/test/java/my.maven.project/AppTest.java
+++ src/test/java/my.maven.project/AppTest.java
@@ -1,7 +1,6 @@
-package my-maven-project;
+package my.maven.project;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 /**
